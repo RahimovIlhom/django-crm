863197192513042722ed7ff2c84b237563db8226
ilhomjon <ilhomjonrahimov217@gmail.com>
2024-03-05 15:24:08 +0500

    admin panel add models

--- course/admin.py
+++ course/admin.py
@@ -1,11 +1,11 @@
-# from django.contrib import admin
-#
-# from .models import Course
-#
-#
-# class CourseAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'created_time']
-#     search_fields = ['title']
-#
-#
-# admin.site.register(Course, CourseAdmin)
+from django.contrib import admin
+
+from .models import Course
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time']
+    search_fields = ['title']
+
+
+admin.site.register(Course, CourseAdmin)
--- group/admin.py
+++ group/admin.py
@@ -1,12 +1,12 @@
-# from django.contrib import admin
-#
-# from .models import Group
-#
-#
-# class GroupAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'course', 'mentor', 'status']
-#     list_filter = ['course', 'status', 'mentor']
-#     search_fields = ['title', 'mentor']
-#
-#
-# admin.site.register(Group, GroupAdmin)
+from django.contrib import admin
+
+from .models import Group
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course', 'mentor', 'status']
+    list_filter = ['course', 'status', 'mentor']
+    search_fields = ['title', 'mentor']
+
+
+admin.site.register(Group, GroupAdmin)
