6f02d7f11ca6cf2c2dca72039e1c8eb8b651399a
ilhomjon <ilhomjonrahimov217@gmail.com>
2024-03-09 01:24:33 +0500

    update lifetime

--- config/settings.py
+++ config/settings.py
@@ -98,8 +98,8 @@ MIDDLEWARE = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
